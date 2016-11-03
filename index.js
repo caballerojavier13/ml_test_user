75587849607fe3c64e2e76ba0636577fe6a4e34a
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2016-11-03 10:55:11 -0300

    Armado de estructura inicial.

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Scapegoat
+ * https://github.com/brentertz/scapegoat
+ *
+ * Copyright (c) 2014 Brent Ertz
+ * Licensed under the MIT license.
+ */
+
+var user = {
+    "username": 'TETE5197722',
+    "email": 'test_user_67445510@testuser.com',
+    "password": 'qatest9130'
+};
+
+
+module.exports = {
+  get_user: function(site) {
+
+    return user;
+    
+  },
+
+  add_account_money: function(amount) {
+    
+    return amount;
+
+  }
+};
